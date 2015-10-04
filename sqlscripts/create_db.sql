3fb96a094121285ef8057de637105cbb1831e70d
Andrii Vynnyk <andrey2901@ua.fm>
2015-10-04 13:04:03 +0300

    Adds sql scripts for creating and dropping the in/outcomings tables.

--- sqlscripts/create_db.sql
+++ sqlscripts/create_db.sql
@@ -1,7 +1,25 @@
 CREATE TABLE store
 (
-	name varchar(40) not null, 
-	price varchar(15) not null, 
+	name varchar(40) not null,
+	price varchar(15) not null,
 	amount varchar(15) not null,
-	CONSTRAINT st_productID PRIMARY KEY (name,price)
-)
+	CONSTRAINT productID PRIMARY KEY (name, price)
+);
+
+CREATE TABLE incomings
+(
+	name varchar(40) not null,
+	price varchar(15) not null,
+	amount varchar(15) not null,
+	incoming_date timestamp not null,
+	CONSTRAINT incomingID PRIMARY KEY (name, price, incoming_date)
+);
+
+CREATE TABLE outcomings
+(
+	name varchar(40) not null,
+	price varchar(15) not null,
+	amount varchar(15) not null,
+	outcoming_date timestamp not null,
+	CONSTRAINT outcomingID PRIMARY KEY (name, price, outcoming_date)
+);
--- sqlscripts/drop_db.sql
+++ sqlscripts/drop_db.sql
@@ -1,2 +1,6 @@
-ALTER TABLE store DROP CONSTRAINT st_productID;
+ALTER TABLE store DROP CONSTRAINT productID;
 DROP TABLE store;
+ALTER TABLE incomings DROP CONSTRAINT incomingID;
+DROP TABLE incomings;
+ALTER TABLE outcomings DROP CONSTRAINT outcomingID;
+DROP TABLE outcomings;
